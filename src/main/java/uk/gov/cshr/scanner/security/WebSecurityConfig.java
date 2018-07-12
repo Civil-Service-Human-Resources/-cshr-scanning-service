98d4869b4cdfed947c01a824e3d51e96d37333d8
seamus <seamus.singh@valtech.co.uk>
2018-07-12 13:22:38 +0100

    CSHRCP-1544 - Initial implementation of security.

---
+++ src/main/java/uk/gov/cshr/scanner/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package uk.gov.cshr.scanner.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private String username;
+    private String password;
+
+    public WebSecurityConfig(@Value("${spring.security.user.name}") String username,
+                             @Value("${spring.security.user.password}") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(password)
+                .roles("SCAN_ROLE");
+    }
+}
